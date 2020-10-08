5f85610d801ecc09a4953ff7adebc6a1e17caa76
Saadani Mohmaed Amine <saadanimohamedamine@gmail.com>
2020-10-09 01:35:27 +0200

    Component in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import imgSrc from './imageInSrc.jpg'
+import imgPub from  "/imageInPublic.PNG"
+import video from '/myVideo.mp4.mp4'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+       <div style={{border:"1px solid black",maxWidth:"100vw"}}>
+         <h1 className="title red">Your name here</h1>
+         <img src={imgSrc} alt="image in src" />
+         <img src={imgPub} alt="image in public" />
+       </div>
+       <video style={{width:"320",height:"240"}} controls>
+         <source src={video} type="video/mp4" />
+       </video>
+    </>
+    
+  )
 }
 
 export default App;
