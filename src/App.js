0b6c5b471660f306602e60f71f4df49a08d31645
Saadani Mohmaed Amine <saadanimohamedamine@gmail.com>
2020-10-09 14:31:36 +0200

    styling and refering to App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import imgSrc from './imageInSrc.jpg'
-import imgPub from  "/imageInPublic.PNG"
-import video from '/myVideo.mp4.mp4'
+import './style.css'
+//import imageInPublic from '../public/imageInPublic.jpg'
+import imageInSrc from './imageInSrc.jpg'
+//import myVideo from '/myVideo.mp4'
 
 function App() {
   return (
-    <>
-       <div style={{border:"1px solid black",maxWidth:"100vw"}}>
-         <h1 className="title red">Your name here</h1>
-         <img src={imgSrc} alt="image in src" />
-         <img src={imgPub} alt="image in public" />
+        
+    <div className="cont">
+       <div style={{border:"1px solid black",maxWidth:"100vw"}} className="div">
+         <h1 className="title red">Your name here :</h1>
+         <h3>Your favorite logo !!</h3>
+         <div className="image-container">
+         <img src={imageInSrc} alt="image in src" className="img"/>
+         <img src={process.env.PUBLIC_URL + "/imageInPublic.jpg"} alt="mypic" className="img"/>
+         </div>
        </div>
-       <video style={{width:"320",height:"240"}} controls>
-         <source src={video} type="video/mp4" />
+       <video style={{width:"400px",height:"420px"}} controls className="video" poster="./logo512.png">
+         <source src={process.env.PUBLIC_URL + "/myVideo.mp4"} />
        </video>
-    </>
+   </div>
+    
     
   )
 }
